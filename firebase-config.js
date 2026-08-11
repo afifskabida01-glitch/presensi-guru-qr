ed5e14476f26533438ec3ad70273e913a7504c02
Loading_Channel <afifskabida01@gmail.com>
2026-08-11 12:27:14 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,11 @@
-// Configuration file for Firebase Integration
-// Dapatkan nilai di bawah ini dari konsol Firebase (Proyek Anda -> Project Settings -> General -> Web Apps)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC1Pp7GpFX3vZ4SVrZ2EFh0gBhi-p9foLU",
   authDomain: "qrpresensi-guru.firebaseapp.com",
@@ -11,5 +16,6 @@ const firebaseConfig = {
   measurementId: "G-PQWQLY9PC7"
 };
 
-// Ekspor ke window agar terbaca oleh app.js
-window.firebaseConfig = firebaseConfig;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
